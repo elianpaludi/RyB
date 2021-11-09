38d9324626ee26f4ecda7b00d1032d6e8ae08a41
BuildTools <elianpaludi@gmail.com>
2021-11-09 20:51:49 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>R&D Constructora</title>
+</head>
+<body>
+    <header>
+        <img src="./img/logoChico.png" alt="">
+        <nav>
+            <ul class="no-responsive">
+                <li><i class="fas fa-home"></i><a href="index.php">Inicio</a></li>
+                <li><i class="fas fa-images"></i><a href="#">Galeria</a></li>
+                <li><i class="fas fa-hammer"></i><a href="#">Servicios</a></li>
+            </ul>
+
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <div class="imgOne"></div>
+            </div>
+            <div class="carousel-item">
+            <div class="imgTwo"></div>
+            </div>
+            <div class="carousel-item">
+            <div class="imgThree"></div>
+            </div>
+        </div>
+        </div>
+    </nav>
+</header>
+
+
+
+    <main>
+        <section class="texto">
+            <h2><div class="logo"></div>Constructora R&D</h2>
+            <h3>
+            Formados por un equipo de profesionales
+            completamente integrados en el proceso creativo, Ingenieros, arquitectos y
+            especialistas que nos permite brindar un servicio personalizado y diferenciado
+            para el Diseño y desarrollo de proyectos de Construcción.
+            </h3>
+        </section>
+
+        <section class="descripciones">
+            <div class="container">
+                <div class="imgDescripciones"></div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+            <div class="container">
+                <div class="imgDescripciones"></div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+            <div class="container">
+                <div class="imgDescripciones"></div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+            <div class="container">
+                <div class="imgDescripciones"></div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+            <div class="container">
+                <div class="imgDescripciones"></div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+            <div class="container">
+                <div class="imgDescripciones"></div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+            <div class="container">
+                <div class="imgDescripciones"></div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+        </section>
+
+
+
+
+
+        <div class="container-maps">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3884.988325700686!2d-72.54715158510439!3d-13.163135967253044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x916d9a5f89555555%3A0x3a10370ea4a01a27!2sMachu%20Picchu!5e0!3m2!1ses!2sar!4v1636491856938!5m2!1ses!2sar" style="border:0;" allowfullscreen="true" loading="lazy"></iframe>
+        </div>
+    </main>
+
+
+    <footer>
+        <div class="footer-container_item">
+            <i class="fas fa-hotel"></i>
+            <div>
+                <p>Urb. Pacífico Mz. I2 Lte. 34 Int. 2</p>
+                <p>NUEVO CHIMBOTE - PERÚ</p>                 
+            </div>
+        </div>
+
+
+        <div class="footer-container_item">
+        <i class="fas fa-clock"></i>
+            <div>
+                <p>Lunes a Sabados</p>
+                <p>08:00 AM - 05:30 PM</p>
+            </div>
+        </div>
+
+
+        <div class="footer-container_item">
+        <i class="fas fa-phone-volume"></i>
+            <div>
+                <p>Teléfono</p>
+                <p>+51 984 070 070</p>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
